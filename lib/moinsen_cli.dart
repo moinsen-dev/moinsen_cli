d8025b10387eb85bf0899e1f35d218e7dc7f9f03
Ulrich Diedrichsen <uli@moinsen.dev>
2024-12-25 12:02:45 +0100

    first commit

---
+++ lib/moinsen_cli.dart
@@ -0,0 +1,10 @@
+/// moinsen_cli, Say moinsen to help you with your flutter projects
+///
+/// ```sh
+/// # activate moinsen_cli
+/// dart pub global activate moinsen_cli
+///
+/// # see usage
+/// moinsen --help
+/// ```
+library;
